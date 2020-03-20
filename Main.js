6af0dc0197a9afcb08d2c32182fc14347cdb1ce6
Kitchefs <62424502+Kitchefs@users.noreply.github.com>
2020-03-20 20:02:13 +1000

    Uploaded Files

---
+++ Main.js
@@ -0,0 +1,20 @@
+function RecipieNamesEqual(x, y) {
+    x = x.toLowerCase();
+    y = y.toLowerCase();
+    x = x.replace(/\s/g, '');
+    y = y.replace(/\s/g, '');
+    return x.includes(y) || y.includes(x) || x == "" || y == "";
+}
+
+function SearchRecipie() {
+    let recipies = document.getElementsByClassName("recipies");
+    let Input = document.getElementById("Search-Box");
+    for (recipie of recipies){
+        if (RecipieNamesEqual(recipie.id, Input.value)) {
+            recipie.style.display = "inline-block";
+        }
+        else {
+            recipie.style.display = "none";
+        }
+    }
+}
