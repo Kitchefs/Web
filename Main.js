a5012e60677672b27043fe491543f4815740f583
Kitchefs <62424502+Kitchefs@users.noreply.github.com>
2020-03-23 09:10:56 +1000

    slide

--- Main.js
+++ Main.js
@@ -9,7 +9,7 @@ function RecipieNamesEqual(x, y) {
 function SearchRecipie() {
     let recipies = document.getElementsByClassName("recipies");
     let Input = document.getElementById("Search-Box");
-    for (recipie of recipies){
+    for (recipie of recipies) {
         if (RecipieNamesEqual(recipie.id, Input.value)) {
             recipie.style.display = "inline-block";
         }
@@ -23,18 +23,14 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000);
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
 }
