8393ae59f58655df6b465878b703a0d57a6ff366
Kitchefs <62424502+Kitchefs@users.noreply.github.com>
2020-03-24 08:54:04 +1000

    .

--- Main.js
+++ Main.js
@@ -18,3 +18,44 @@ function SearchRecipie() {
         }
     }
 }
+
+const duration = document.getElementById("cd-duration").innerHTML * 60;
+let ticker;
+
+function ResetTimer() {
+    clearInterval(ticker)
+    time = duration;
+    document.getElementById("cd-min").innerHTML = time;
+    document.getElementById("cd-sec").innerHTML = 0;
+}
+
+function CookTimer() {
+    ResetTimer();
+
+    let min = document.getElementById("cd-min");
+    let sec = document.getElementById("cd-sec");
+
+    let time = duration;
+    let secs = time;
+    let mins = Math.floor(secs / 60);
+    secs -= mins * 60;
+
+    min.innerHTML = mins;
+    sec.innerHTML = (secs == 0 ? "00" : secs);
+
+    ticker = setInterval(function () {
+        time--;
+
+        let secs = time;
+        let mins = Math.floor(secs / 60);
+        secs -= mins * 60;
+
+        min.innerHTML = mins;
+        sec.innerHTML = secs;
+
+        if (time <= 0) {
+            // Sound here
+            ResetTimer();
+        }
+    }, 1000);
+}
