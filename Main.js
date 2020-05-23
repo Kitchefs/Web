437d294ee1facf634f4c64508f795927ea07ea49
Kitchefs <62424502+Kitchefs@users.noreply.github.com>
2020-05-23 11:27:06 +1000

    Fix Spelling of Recipe

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-function RecipieNamesEqual(x, y) {
+function RecipeNamesEqual(x, y) {
     x = x.toLowerCase();
     y = y.toLowerCase();
     x = x.replace(/\s/g, '');
@@ -6,11 +6,11 @@ function RecipieNamesEqual(x, y) {
     return x.includes(y) || y.includes(x) || x == "" || y == "";
 }
 
-function SearchRecipie() {
+function SearchRecipe() {
     let recipies = document.getElementsByClassName("recipies");
     let Input = document.getElementById("Search-Box");
     for (recipie of recipies) {
-        if (RecipieNamesEqual(recipie.id, Input.value)) {
+        if (RecipeNamesEqual(recipie.id, Input.value)) {
             recipie.style.display = "inline-block";
         }
         else {
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Kitchefs is a website were you can find all the best recipies all in one place
 ### Quality
 I have made all of the recipies and guarantee they work. The pictures are mine.
 ```
-'The Most Epic Recipies In The World' - Me 2020
+'The Most Epic Recipes In The World' - Me 2020
 ```
 ### Sugestions
 Any suggestions email EpicKitchefs@gmail.com
